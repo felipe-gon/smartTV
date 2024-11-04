376f2e30c80b9beb4a31e4f3a14526f1b5eac8e8
felipe-gon <felipev2014@hotmail.com>
2024-11-04 20:44:11 -0300

    Complemento

--- src/User.java
+++ src/User.java
@@ -2,10 +2,26 @@ public class User {
     public static void main(String[] args) {
         smartTv smartTv = new smartTv();
 
-
         System.out.println("A TV está ligada? "+ smartTv.ligada);
         System.out.println("Canal atual: "+ smartTv.canal);
         System.out.println("Volume atual: "+ smartTv.volume);
 
+        smartTv.aumentaVolume();
+        System.out.println("Volume atual: "+ smartTv.volume);
+
+        smartTv.desligar();
+        System.out.println("A TV está ligada? "+ smartTv.ligada);
+
+        smartTv.ligar();
+        System.out.println("A TV está ligada? "+ smartTv.ligada);
+
+        smartTv.diminuirCanal();
+        System.out.println("Canal atual: "+ smartTv.canal);
+
+        smartTv.aumentarCanal();
+        System.out.println("Canal atual: "+smartTv.canal);
+
+        smartTv.setarCanal(435);
+        System.out.println("Canal selecionado: "+smartTv.canal);
     }
 }
--- src/smartTv.java
+++ src/smartTv.java
@@ -1,5 +1,28 @@
 public class smartTv {
     boolean ligada = true;
     int volume = 10;
-    int canal = 1;
+    int canal = 5;
+
+    public void aumentaVolume() {
+        volume++;
+    }
+    public void ligar() {
+        ligada = true;
+    }
+    public void desligar() {
+        ligada = false;
+    }
+    public void aumentarCanal() {
+        canal++;
+    }
+    public void diminuirCanal() {
+        canal--;
+    }
+    public void setarCanal(int mudaCanal) {
+        canal = mudaCanal;
+        if (canal > 50)
+            System.out.println("Canal inválido, deve ser menor que 50!");
+        else if (canal < 1)
+            System.out.println("Canal inválido, deve ser maior que 0!");
+    }
 }
